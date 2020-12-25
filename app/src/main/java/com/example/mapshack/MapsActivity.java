9d56bf9e5e05974cd32e11f4ec96134bb1385d5e
afds <a@a.com>
2020-12-25 16:58:04 +0500

    Added OnClick to marker

--- app/src/main/java/com/example/mapshack/MapsActivity.java
+++ app/src/main/java/com/example/mapshack/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.mapshack;
 
-import androidx.fragment.app.FragmentActivity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -8,9 +9,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -24,22 +26,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tyumen = new LatLng(57.142357545110094, 65.58988839556109);
+        Marker marker = mMap.addMarker(new MarkerOptions().position(tyumen).title("Магнит"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tyumen));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                openPlanActivity();
+                return true;
+            }
+        });
+    }
+
+    public void openPlanActivity() {
+        Intent intent = new Intent(this, PlanLayout.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mapshack/PlanLayout.java
@@ -0,0 +1,13 @@
+package com.example.mapshack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlanLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plan_layout);
+    }
+}
